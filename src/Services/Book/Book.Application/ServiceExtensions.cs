eb592c79f3ceb49d343aed0fb6cae04d18ae0449
Shidlovsky Roman <shidlovskyroman@gmail.com>
2024-03-26 01:17:21 +0300

    Init project structure

---
+++ src/Services/Book/Book.Application/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Book.Application;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureApplication(this IServiceCollection services)
+    {
+        
+    }
+}
