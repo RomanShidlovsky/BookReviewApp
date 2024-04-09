b81059a2b2adde67f3f0ad51a439f52eaa52c84c
Roman Shidlovsky <shidlovskyroman@gmail.com>
2024-04-09 15:38:40 +0300

    Refactor get books query handler

--- src/Services/Book/Book.Application/Features/BookFeatures/Queries/GetAll/GetAllBooksQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using Book.Application.DTOs.Book.ResponseDTOs;
-using Book.Application.Interfaces.Queries;
-
-namespace Book.Application.Features.BookFeatures.Queries.GetAll;
-
-public sealed record GetAllBooksQuery : IQuery<BookResponseDto>;
