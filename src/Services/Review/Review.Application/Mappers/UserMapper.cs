1abc637d56651768b309ecc14714a8ad197eda87
Roman Shidlovsky <shidlovskyroman@gmail.com>
2024-04-17 21:44:37 +0300

    Add mapper profiles

---
+++ src/Services/Review/Review.Application/Mappers/UserMapper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Review.Application.DTOs.RequestDTOs;
+using Review.Application.DTOs.ResponseDTOs;
+using Review.Domain.Entities;
+
+namespace Review.Application.Mappers;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<CreateUserDto, User>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+            .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl));
+        
+        CreateMap<UpdateUserDto, User>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+            .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl));
+
+        CreateMap<User, UserResponseDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+            .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl));
+    }
+}
