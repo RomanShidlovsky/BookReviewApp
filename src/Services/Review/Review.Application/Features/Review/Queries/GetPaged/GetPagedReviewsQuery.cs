7f0ea78a3d3fa51aca217ce4d9dfd11a31411367
Roman Shidlovsky <shidlovskyroman@gmail.com>
2024-04-17 11:24:08 +0300

    Implement review queries

---
+++ src/Services/Review/Review.Application/Features/Review/Queries/GetPaged/GetPagedReviewsQuery.cs
@@ -0,0 +1,11 @@
+﻿using Review.Application.DTOs.ResponseDTOs;
+using Review.Application.Interfaces.Queries;
+
+namespace Review.Application.Features.Review.Queries.GetPaged;
+
+public sealed record GetPagedReviewsQuery(
+    string FilterQueryString,
+    string OrderByQueryString,
+    int PageNumber,
+    int PageSize)
+    : IQuery<ReviewResponseDto>;
---
+++ src/Services/Review/Review.Application/Features/Review/Queries/GetPaged/GetPagedReviewsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Review.Application.DTOs.ResponseDTOs;
+using Review.Application.Interfaces.Queries;
+using Review.Domain.Interfaces.Repositories;
+using Shared.Wrappers;
+
+namespace Review.Application.Features.Review.Queries.GetPaged;
+
+public class GetPagedReviewsQueryHandler(IReviewRepository _repository, IMapper _mapper)
+    : IQueryHandler<GetPagedReviewsQuery, ReviewResponseDto>
+{
+    public async Task<Response<IEnumerable<ReviewResponseDto>>> Handle(GetPagedReviewsQuery request, CancellationToken cancellationToken)
+    {
+        var reviews = await _repository.GetPagedAsync(request.PageNumber, request.PageSize,
+            request.FilterQueryString, request.OrderByQueryString, cancellationToken);
+
+        return _mapper.Map<List<ReviewResponseDto>>(reviews);
+    }
+}
