170fc16cec4560ca8b85f476eb1e763d7ee2f9a5
Roman Shidlovsky <shidlovskyroman@gmail.com>
2024-04-24 15:20:32 +0300

    Add Swagger for Ocelot

--- src/ApiGateways/Ocelot.ApiGateway/Program.cs
+++ src/ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -10,19 +10,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.ConfigureServices(builder.Configuration, builder.Environment);
 builder.Services.ConfigureLogging(builder, Assembly.GetExecutingAssembly().GetName().Name!);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.ConfigureSwagger(builder.Configuration);
+//builder.Services.ConfigureSwagger(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-if (!app.Environment.IsProduction())
+/*if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
 
-app.UseHttpsRedirection();
+app.UseSwaggerForOcelotUI(opt => opt.PathToSwaggerGenerator = "/swagger/docs");
 app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.All });
 app.UseCors("CorsPolicy");
 await app.UseAuthentication().UseOcelot();
