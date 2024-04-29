69e7f570ab73fcfca6d526d7e219a5d32f12668e
Roman Shidlovsky <shidlovskyroman@gmail.com>
2024-04-29 16:38:49 +0300

    Implement caching in repositories of Book service

--- src/Services/Book/Book.Infrastructure/Repositories/BookRepository.cs
+++ src/Services/Book/Book.Infrastructure/Repositories/BookRepository.cs
@@ -2,12 +2,20 @@
 using Book.Infrastructure.Context;
 using Book.Infrastructure.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Shared.Cache;
 using BookEntity = Book.Domain.Entities.Book;
 
 namespace Book.Infrastructure.Repositories;
 
-public class BookRepository(BookContext context) : BaseRepository<BookEntity>(context), IBookRepository
+public class BookRepository(BookContext context, IDistributedCache _cache)
+    : BaseRepository<BookEntity>(context), IBookRepository
 {
+    public const string BaseCacheKey = "Book";
+    public const string BooksCacheKey = "Books";
+
+    public static List<string> CacheKeys = [];
+
     protected override IQueryable<BookEntity> GetEntitySet()
     {
         return base.GetEntitySet()
@@ -16,20 +24,149 @@ public class BookRepository(BookContext context) : BaseRepository<BookEntity>(co
             .Include(b => b.Languages);
     }
 
-    public Task<BookEntity?> GetByOpenLibraryKeyAsync(string key, CancellationToken cancellationToken)
-    {
-        return GetEntitySet()
-            .FirstOrDefaultAsync(b => b.OpenLibraryKey != null && b.OpenLibraryKey == key, 
-                cancellationToken);
-    }
-
-    public Task<List<BookEntity>> GetBooksAsync(int pageNumber, int pageSize, string filterQueryString,
+    public async Task<List<BookEntity>> GetBooksAsync(int pageNumber, int pageSize, string filterQueryString,
         string orderByQueryString, CancellationToken cancellationToken)
     {
-        return GetEntitySet()
-            .Filter(filterQueryString)
-            .Sort(orderByQueryString)
-            .Paginate(pageNumber, pageSize)
-            .ToListAsync(cancellationToken);
+        var cacheKey = BooksCacheKey + pageNumber + pageSize + filterQueryString + orderByQueryString;
+
+        var booksCache = await _cache.GetAsync(cacheKey, cancellationToken);
+
+        List<BookEntity>? books;
+
+        if (booksCache is not null)
+        {
+            books = Cache<List<BookEntity>>.GetData(booksCache);
+        }
+        else
+        {
+            books = await GetEntitySet()
+                .Filter(filterQueryString)
+                .Sort(orderByQueryString)
+                .Paginate(pageNumber, pageSize)
+                .ToListAsync(cancellationToken);
+
+            booksCache = Cache<List<BookEntity>>.GetCache(books, out var options);
+
+            await _cache.SetAsync(cacheKey, booksCache, options, cancellationToken);
+
+            CacheKeys.Add(cacheKey);
+        }
+
+        return books;
+    }
+
+    public override async Task<BookEntity?> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        var cacheKey = BaseCacheKey + id;
+
+        var bookCache = await _cache.GetAsync(cacheKey, cancellationToken);
+
+        BookEntity? book;
+
+        if (bookCache is not null)
+        {
+            book = Cache<BookEntity>.GetData(bookCache);
+        }
+        else
+        {
+            book = await base.GetByIdAsync(id, cancellationToken);
+
+            bookCache = Cache<BookEntity>.GetCache(book, out var options);
+
+            await _cache.SetAsync(cacheKey, bookCache, options, cancellationToken);
+        }
+
+        return book;
+    }
+
+    public async Task<BookEntity?> GetByOpenLibraryKeyAsync(string key, CancellationToken cancellationToken)
+    {
+        var cacheKey = BaseCacheKey + key;
+
+        var bookCache = await _cache.GetAsync(cacheKey, cancellationToken);
+
+        BookEntity? book;
+
+        if (bookCache is not null)
+        {
+            book = Cache<BookEntity>.GetData(bookCache);
+        }
+        else
+        {
+            book = await GetEntitySet()
+                .FirstOrDefaultAsync(b => b.OpenLibraryKey != null && b.OpenLibraryKey == key,
+                    cancellationToken);
+
+            bookCache = Cache<BookEntity>.GetCache(book, out var options);
+
+            await _cache.SetAsync(cacheKey, bookCache, options, cancellationToken);
+        }
+
+        return book;
+    }
+
+    public override void Create(BookEntity entity)
+    {
+        base.Create(entity);
+        
+        RemoveBooksCache();
+    }
+
+    public override void Update(BookEntity entity)
+    {
+        base.Update(entity);
+        
+        RemoveCache(entity.Id, entity.OpenLibraryKey);
+    }
+
+    public override void Delete(BookEntity entity)
+    {
+        base.Delete(entity);
+        
+        RemoveCache(entity.Id, entity.OpenLibraryKey);
+    }
+
+    private void RemoveCache(int id, string? key)
+    {
+        _cache.Remove(BaseCacheKey + id);
+
+        if (key is not null)
+        {
+            _cache.Remove(BaseCacheKey + key);
+        }
+        
+        RemoveBooksCache();
+    }
+
+    private async Task RemoveCacheAsync(int id, string? key, CancellationToken cancellationToken)
+    {
+        await _cache.RemoveAsync(BaseCacheKey + id, cancellationToken);
+
+        if (key is not null)
+        {
+            await _cache.RemoveAsync(BaseCacheKey + key, cancellationToken);
+        }
+        
+        await RemoveBooksCacheAsync(cancellationToken);
+    }
+
+    private void RemoveBooksCache()
+    {
+        foreach (var key in CacheKeys)
+        {
+            _cache.Remove(key);
+        }
+
+        CacheKeys.Clear();
+    }
+    
+    private async Task RemoveBooksCacheAsync(CancellationToken cancellationToken)
+    {
+        foreach (var key in CacheKeys)
+        {
+           await _cache.RemoveAsync(key, cancellationToken);
+        }
+
+        CacheKeys.Clear();
     }
 }
--- src/Services/Book/Book.Infrastructure/Repositories/LanguageRepository.cs
+++ src/Services/Book/Book.Infrastructure/Repositories/LanguageRepository.cs
@@ -3,34 +3,128 @@ using Book.Domain.Extensions;
 using Book.Domain.Interfaces.Repositories;
 using Book.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Cache;
 using BookEntity = Book.Domain.Entities.Book;
 
 namespace Book.Infrastructure.Repositories;
 
-public class LanguageRepository(BookContext context) : BaseRepository<Language>(context), ILanguageRepository
+public class LanguageRepository(BookContext context, IDistributedCache _cache)
+    : BaseRepository<Language>(context), ILanguageRepository
 {
+    private const string BaseCacheKey = "Language";
+    private const string AllLanguagesKey = "AllLanguages";
+
     private IQueryable<BookEntity> GetBookSet()
     {
         return Context.Set<BookEntity>()
             .Where(b => b.DateDeleted == null);
     }
 
-    public Task<Language?> GetByNameAsync(string name, CancellationToken cancellationToken)
+    public override async Task<List<Language>> GetAllAsync(CancellationToken cancellationToken)
     {
-        return GetEntitySet()
-            .FirstOrDefaultAsync(l => l.Name == name, cancellationToken);
+        var languagesCache = await _cache.GetAsync(AllLanguagesKey, cancellationToken);
+
+        List<Language>? languages;
+
+        if (languagesCache is not null)
+        {
+            languages = Cache<List<Language>>.GetData(languagesCache);
+        }
+        else
+        {
+            languages = await base.GetAllAsync(cancellationToken);
+
+            languagesCache = Cache<List<Language>>.GetCache(languages, out var options);
+
+            await _cache.SetAsync(AllLanguagesKey, languagesCache, options, cancellationToken);
+        }
+
+        return languages;
+    }
+
+    public override async Task<Language?> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        var cacheKey = BaseCacheKey + id;
+
+        var languageCache = await _cache.GetAsync(cacheKey, cancellationToken);
+
+        Language? language;
+
+        if (languageCache is not null)
+        {
+            language = Cache<Language>.GetData(languageCache);
+        }
+        else
+        {
+            language = await base.GetByIdAsync(id, cancellationToken);
+
+            languageCache = Cache<Language>.GetCache(language, out var options);
+
+            await _cache.SetAsync(cacheKey, languageCache, options, cancellationToken);
+        }
+
+        return language;
+    }
+
+    public async Task<Language?> GetByNameAsync(string name, CancellationToken cancellationToken)
+    {
+        var cacheKey = BaseCacheKey + name;
+
+        var languageCache = await _cache.GetAsync(cacheKey, cancellationToken);
+
+        Language? language;
+
+        if (languageCache is not null)
+        {
+            language = Cache<Language>.GetData(languageCache);
+        }
+        else
+        {
+            language = await GetEntitySet()
+                .FirstOrDefaultAsync(l => l.Name == name, cancellationToken);
+
+            languageCache = Cache<Language>.GetCache(language, out var options);
+
+            await _cache.SetAsync(cacheKey, languageCache, options, cancellationToken);
+        }
+
+        return language;
+    }
+
+    public override void Create(Language entity)
+    {
+        base.Create(entity);
+        
+        _cache.Remove(AllLanguagesKey);
+    }
+
+    public override void Update(Language entity)
+    {
+        base.Update(entity);
+        
+        RemoveLanguageCache(entity.Id, entity.Name);
+    }
+
+    public override void Delete(Language entity)
+    {
+        base.Delete(entity);
+        
+        RemoveLanguageCache(entity.Id, entity.Name);
     }
 
     public async Task<bool> AddLanguageToBookAsync(int languageId, int bookId, CancellationToken cancellationToken)
     {
         var book = await GetBookSet()
             .FirstAsync(b => b.Id == bookId, cancellationToken);
-        
+
         var language = await GetEntitySet()
             .FirstAsync(l => l.Id == languageId, cancellationToken);
-        
+
         book.Languages.Add(language);
+
+        await RemoveBookCacheAsync(book.Id, book.OpenLibraryKey, cancellationToken);
 
         return true;
     }
@@ -39,10 +133,29 @@ public class LanguageRepository(BookContext context) : BaseRepository<Language>(
     {
         var book = await GetBookSet()
             .FirstAsync(b => b.Id == bookId, cancellationToken);
-        
+
         var language = await GetEntitySet()
             .FirstAsync(l => l.Id == languageId, cancellationToken);
+
+        await RemoveBookCacheAsync(book.Id, book.OpenLibraryKey, cancellationToken);
         
         return book.Languages.Remove(language);
+    }
+
+    private async Task RemoveBookCacheAsync(int id, string? key, CancellationToken cancellationToken)
+    {
+        await _cache.RemoveAsync(BookRepository.BaseCacheKey + id, cancellationToken);
+
+        if (key is not null)
+        {
+            await _cache.RemoveAsync(BookRepository.BaseCacheKey + key, cancellationToken);
+        }
+    }
+    
+    private void RemoveLanguageCache(int id, string name)
+    {
+        _cache.RemoveAsync(BaseCacheKey + id);
+        _cache.RemoveAsync(BaseCacheKey + name);
+        _cache.Remove(AllLanguagesKey);
     }
 }
